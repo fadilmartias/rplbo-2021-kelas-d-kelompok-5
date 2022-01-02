f1d7b52f928bc20210fc7c4fd4c36677b5796604
fadilmartias <fadilmartias26@gmail.com>
2022-01-03 00:05:30 +0700

    update user

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,18 +15,39 @@ class UserSeeder extends Seeder
     public function run()
     {
         User::create([
-            'nama_user' => 'Padil',
+            'nama_user' => 'M. Fadil Martias',
             'nip' => '261201',
-            'email' => 'padel@hah.com',
-            'password' => bcrypt('pitra'),
+            'email' => 'kepsek@gmail.com',
+            'password' => bcrypt('kepsek123'),
             'role' => 'Kepala Sekolah'
         ]);
         User::create([
-            'nama_user' => 'Padil2',
-            'nip' => '261201',
-            'email' => 'padel2@hah.com',
-            'password' => bcrypt('pitra'),
+            'nama_user' => 'Imaduddien',
+            'nip' => '524311',
+            'email' => 'keptu@gmail.com',
+            'password' => bcrypt('keptu123'),
+            'role' => 'Kepala TU'
+        ]);
+        User::create([
+            'nama_user' => 'Felina Amelia',
+            'nip' => '974512',
+            'email' => 'staff@gmail.com',
+            'password' => bcrypt('staff123'),
             'role' => 'Staff TU'
+        ]);
+        User::create([
+            'nama_user' => 'Eka Suryani',
+            'nip' => '836382',
+            'email' => 'staff2@gmail.com',
+            'password' => bcrypt('staff1234'),
+            'role' => 'Staff TU'
+        ]);
+        User::create([
+            'nama_user' => 'M. Taufikur Rahman',
+            'nip' => '847492',
+            'email' => 'resepsionis@gmail.com',
+            'password' => bcrypt('resepsionis123'),
+            'role' => 'Resepsionis'
         ]);
     }
 }
